aa553c07069eb975bf6edbffdc9f46a203fbe39c
realstresser <rezaebr3234s4@gmail.com>
2025-02-15 17:40:36 +0300

    update

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
-// getting repo
 document.addEventListener('DOMContentLoaded', function() {
     const repoNumberElement = document.getElementById('repo-number');
 
-    fetch('https://api.github.com/orgs/XenonCommunity/repos')
+    fetch('https://api.github.com/orgs/SyNdicateFoundation/repos')
         .then(response => response.json())
         .then(data => {
-            const repoCount = data.length;
-            repoNumberElement.textContent = repoCount;
-            console.info("repos loaded - script.js")
-            console.warn("Website created by kilosheet")
+            repoNumberElement.textContent = data.length;
         })
         .catch(error => {
             console.error('Error fetching repository count:', error);
