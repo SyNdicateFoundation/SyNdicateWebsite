ade78638d31d1b59be3f098e72eca1540dce10d0
realstresser <rezaebr3234s4@gmail.com>
2025-02-15 19:40:06 +0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const repoNumberElement = document.getElementById('repo-number');
 
-    fetch('https://api.github.com/orgs/SyNdicateFoundation/repos')
-        .then(response => response.json())
-        .then(data => {
-            repoNumberElement.textContent = data.length;
-        })
-        .catch(error => {
-            console.error('Error fetching repository count:', error);
-            repoNumberElement.textContent = 'Error';
-        });
-});
